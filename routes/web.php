ef34a1231bc43a84a15d7dfec9bddc85cdca2e78
Yang shu mei <pbs71129@gmail.com>
2022-12-25 22:06:32 +0800

    Route 設置

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\HTTP\Controllers\HomeController;
+use APP\HTTP\Controllers\AboutController;
+use APP\HTTP\Controllers\NewsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',[\App\Http\Controllers\HomeController::class,'index'])->name('home.index');
+Route::get('about',[\App\Http\Controllers\AboutController::class,'index'])->name('about.index');
+Route::get('news',[\App\Http\Controllers\NewsController::class,'index'])->name('news.index');
